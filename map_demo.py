4083ea937883d7959f2e1246f171a0728642f9b7
Carlos D. Rubido <cdrubido@live.unc.edu>
2019-12-01 09:04:51 -0500

    Multiprocessing demo, pool.map()

--- map_demo.py
+++ map_demo.py
@@ -1,6 +1,6 @@
 from inmutable_data_structure import scientist
 
-name_and_ages = tuple(map(lambda x: {'name': x.name, 'age': 2017 - x.born}, scientist))
+name_and_ages = tuple(map(lambda x: {'name': x.name, 'age': 2019 - x.born}, scientist))
 
 for i in name_and_ages:
     print(i)
---
+++ multiprocessing/multiprocessing_demo.py
@@ -0,0 +1,46 @@
+import collections
+import multiprocessing
+import os
+import time
+from pprint import pprint
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel'
+])
+
+scientist = (
+    Scientist(name='Ada Lovelance', field='Math', born=1815, nobel=False),
+    Scientist(name='Emmy Noethere', field='Math', born=1882, nobel=False),
+    Scientist(name='Marie Curie', field='Physics', born=1867, nobel=True),
+    Scientist(name='To Youyou', field='Chemistry', born=1930, nobel=True),
+    Scientist(name='Ada Yonath', field='Chemistry', born=1939, nobel=True),
+    Scientist(name='Vera Rubin', field='Astronomy', born=1928, nobel=False),
+    Scientist(name='Sally Ride', field='Physics', born=1951, nobel=False),
+)
+
+def transform(x):
+    print(f'Process {os.getpid()} working on record {x.name}')
+    time.sleep(2)
+    result = {'name': x.name, 'age': 2019 - x.born}
+    print(f'Process {os.getpid()} done processing {x.name}')
+    return result
+
+if __name__ =='__main__':
+    pprint(scientist)
+    print('__________________________________________________________________________')
+
+    start_time = time.time()
+
+    pool = multiprocessing.Pool(processes=7, maxtasksperchild=1)
+    scientist_ages = pool.map(transform, scientist)
+
+    # First try it without multiprocessing(uncoment bellow, comment above)
+    # scientist_ages = tuple(map(transform, scientist))
+
+    end_time = time.time()
+
+    print(f'\nTime to complete: {end_time - start_time:.2f}s\n')
+    print(scientist_ages)
